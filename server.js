7aa60023ff43e7e5293e2d9f06359aa27ac144f7
Daniel <daniel-mcintyre@hotmail.com>
2022-09-01 11:54:14 -0400

    intial graphQL setup in backend

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const { graphqlHTTP } = require("express-graphql");
+const { GraphQLSchema, GraphQLObjectType, GraphQLString} = require('graphql');
+
 const app = express();
 
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'Daniel',
+        fields: () => ({
+            message: {
+                type: GraphQLString,
+                resolve: () => 'Hello World'
+            }
+        })
+    })
+});
+
 app.use('/graphql', graphqlHTTP({
-    graphiql: true
+    schema: schema,
+    graphiql: true,
 }))
 app.listen(4000, () => console.log('server running'));
